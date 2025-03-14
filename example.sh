c9da96c2a6dc4a727d3ae99b3887759045516584
CoinBR <coinbr@gmail.com>
2025-03-14 02:12:29 -0300

    create first working version

---
+++ example.sh
@@ -0,0 +1,29 @@
+!/bin/sh
+
+# points to script dir
+DIR="$(dirname "$0")"
+
+# ! REQUIRED !
+# Source/Load the Megaman library
+. ${DIR}/megaman.sh
+
+
+# REQUIRED!
+# Default action when no arguments are provided
+default_action() {
+    error "Usage: bla bla bla"
+}
+
+process_fruits() {
+    process_item 'apple' 'banana' 'kiwi'
+}
+
+process_item() {
+  for port in "$@"; do
+    echo "Processing item: $1"
+  done
+}
+
+# ! REQUIRED !
+# run the functions passed as arguments
+run_args "$@"
